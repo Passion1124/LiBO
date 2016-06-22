3801436f3d3cbbcda1796807f817f1a0ea37fbbf
Passion <330352812@qq.com>
2016-06-22 12:11:30 +0800

    first work

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,25 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var result = [];
+  var obj = {};
+  for (var i = 0; i <collection.length; i++){
+    if(collection[i].length != 1){
+      var coll = collection[i].split("-");
+      if(!obj[coll[0]]){
+        obj[coll[0]] = parseInt(coll[1]);
+      }
+    }else {
+      if (!obj[collection[i]]){
+        obj[collection[i]] = 1;
+      }else {
+        obj[collection[i]]++;
+      }
+    }
+  }
+  for (var i in obj){
+    result.push({key:i,count:obj[i]});
+  }
+  return result;
 }
 
 module.exports = count_same_elements;
--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,5 +1,38 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var result = [];
+  var obj = {};
+  for (var i = 0 ; i < collection.length; i++){
+    if (collection[i].length != 1){
+      var coll,num;
+      if (collection[i].indexOf("[") != -1){
+        coll = collection[i].split("[");
+        num = parseInt(coll[1].substring(0,coll[1].length-1));
+      }else if (collection[i].indexOf("-") != -1){
+        coll = collection[i].split("-");
+        num = parseInt(coll[1]);
+      }else if (collection[i].indexOf(":") != -1){
+        coll = collection[i].split(":");
+        num = parseInt(coll[1]);
+      }
+
+      if (!obj[coll[0]]){
+        obj[coll[0]] = num;
+      }else {
+        obj[coll[0]]+=num;
+      }
+    }else {
+      if (!obj[collection[i]]){
+        obj[collection[i]] = 1;
+      }else {
+        obj[collection[i]]++;
+      }
+    }
+  }
+  for (var i in obj){
+    result.push({name:i,summary:obj[i]});
+  }
+  return result;
 }
 
 module.exports = count_same_elements;
