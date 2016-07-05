58cd71a680b9981df9cbb35a214566f501759676
Passion <330352812@qq.com>
2016-07-05 14:30:25 +0800

    用循环和判断做数据的转换

--- practices/section_3/practice_1/practice.js
+++ practices/section_3/practice_1/practice.js
@@ -1,5 +1,15 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var result = [];
+  for (var i = 0 ; i < collection_a.length; i++){
+    for (var j = 0 ; j < object_b.value.length; j++){
+      if(collection_a[i].key == object_b.value[j]){
+        collection_a[i].count --;
+      }
+    }
+    result.push(collection_a[i]);
+  }
+  return result;
 }
 
 module.exports = create_updated_collection;
--- practices/section_3/practice_4/practice.js
+++ practices/section_3/practice_4/practice.js
@@ -1,5 +1,36 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var result = [];
+  var obj = {};
+  var collection_c = [];
+  for (var i = 0 ; i < collection_a.length ; i++){
+    if(collection_a[i].indexOf("-") != -1){
+      var key = collection_a[i].substring(0,collection_a[i].indexOf("-"));
+      var count = collection_a[i].substring(collection_a[i].indexOf("-") + 1);
+      obj[key] = count;
+    }else {
+      if(!obj[collection_a[i]]){
+        obj[collection_a[i]] = 1;
+      }else {
+        obj[collection_a[i]]++;
+      } 
+    }
+  }
+  for (var i in obj){
+    collection_c.push({key:i,count:obj[i]});
+  }
+  for (var i = 0; i < collection_c.length; i++){
+    for (var j = 0 ; j < object_b.value.length; j++){
+      if(collection_c[i].key == object_b.value[j]){
+        if(collection_c[i].count >= 3){
+          collection_c[i].count -= parseInt(collection_c[i].count/3);
+        }
+      }
+    }
+    result.push(collection_c[i]);
+  }
+  return result;
+  
 }
 
 module.exports = create_updated_collection;
